e1702a12c2aff88dbf6ec7b368e9efa7d77a33d9
Edward Tashjian <luekwa@gmail.com>
2015-12-01 15:38:04 -0600

    Added checking to simple_band

--- test/simple_band.cpp
+++ test/simple_band.cpp
@@ -1,7 +1,5 @@
-#include <db/cholesky_band.h>
-#include <db/cholesky_eigen.h>
-#include <db/band_matrix_type.h>
 #include <db/eigen_util.h>
+#include <db/cholesky_eigen.h>
 #include <db/common.h>
 #include <db/timer.h>
 #include <Eigen/Core>
@@ -24,6 +22,14 @@ int main()
     _myTimer.stopTimer();
     std::cout << "Elapsed Time to Create Input SPDB Matrix : " << _myTimer.elapsedInSec() << " second\n";
 
+    // Compute reference case
+    cout << "Computing Reference Case... " << flush;
+    MatrixXf A_ref = band_to_eigen(A);
+    MatrixXf L_ref = MatrixXf::Zero(dim, dim);
+    MatrixXf D_ref = MatrixXf::Zero(dim, dim);
+    cholesky_eigen_serial(A_ref, L_ref, D_ref);
+    cout << "done" << endl;
+
     // allocate L and D and compute decomp
     cout << "Computing serial decomposition... " << endl;
     BandMatrix L = createEmptyBandMatrix(dim, bandwidth);
@@ -34,26 +40,20 @@ int main()
     _myTimer.stopTimer();
     std::cout << "Elapsed Time to Perform Cholesky Decomposition on Input SPDB Matrix : " << _myTimer.elapsedInSec() << " second\n";
 
+    // compare results
+    unsigned diff = cmp_matrices(L_ref, L);
+    diff += cmp_matrices(D_ref, D);
+    if(diff) cout << "FAILED" << endl;
+    else cout << "PASSED" << endl;
+
+
     _myTimer.startTimer();
     cholesky_band_serial_index_handling(A, L, D);
     _myTimer.stopTimer();
-<<<<<<< HEAD
-    std::cout << "Elapsed Time to Perform Cholesky Decomposition on Input SPDB Matrix : " << _myTimer.elapsedInSec() << " second\n";
-=======
     std::cout << "Elapsed Time to Perform Cholesky Decomposition on Input SPDB Matrix : " << _myTimer.elapsedInSec() << " second. Speedup=" << _myTimer.cummulativeSpeedup() << "x\n";
 
-    /*
-    // check results
-    cout << "Comparing results... " << flush;
-    int differences = cmp_matrices(result, A);
-    if(differences == 0)
-    {
-    cout << "PASSED!" << endl;
-    }
-    else
-    {
-    cout << "FAILED! - " << differences << " differences" << endl;
-    }
-    */
->>>>>>> fa65e8062d7b0853d2d340a9f54a6c9ff7b0c27f
+    diff = cmp_matrices(L_ref, L);
+    diff += cmp_matrices(D_ref, D);
+    if(diff) cout << "FAILED" << endl;
+    else cout << "PASSED" << endl;
 }
