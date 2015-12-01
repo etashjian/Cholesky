225db7bf7267bcc7afe56ed6211b19b089da8d5b
DAOHANG SHI <dshi7@euler.wacc.wisc.edu>
2015-12-01 15:08:23 -0600

    add timer in simple_band.cpp

--- test/simple_band.cpp
+++ test/simple_band.cpp
@@ -1,4 +1,5 @@
 #include <db/common.h>
+#include <db/timer.h>
 #include <Eigen/Core>
 #include <iostream>
 
@@ -7,31 +8,38 @@ using namespace Eigen;
 
 int main()
 {
-  dim_t dim = 10;
-  dim_t bandwidth = 1;
+    MyTimer _myTimer;
+    dim_t dim = 10;
+    dim_t bandwidth = 1;
 
-  // generate random input matrix
-  cout << "Generating input... " << flush;
-  srand(time(NULL));
-  BandMatrix A = createSymmetricPositiveDefiniteBandMatrix(dim, bandwidth);
+    // generate random input matrix
+    cout << "Generating input... " << flush;
+    _myTimer.startTimer();
+    srand(time(NULL));
+    BandMatrix A = createSymmetricPositiveDefiniteBandMatrix(dim, bandwidth);
+    _myTimer.stopTimer();
+    std::cout << "Elapsed Time to Create Input SPDB Matrix : " << _myTimer.elapsedInSec() << " second\n";
 
-  // allocate L and D and compute decomp
-  cout << "Computing serial decomposition... " << endl;
-  BandMatrix L = createEmptyBandMatrix(dim, bandwidth);
-  BandMatrix D = createEmptyBandMatrix(dim, bandwidth);
-  cholesky_band_serial(A, L, D);
+    // allocate L and D and compute decomp
+    cout << "Computing serial decomposition... " << endl;
+    BandMatrix L = createEmptyBandMatrix(dim, bandwidth);
+    BandMatrix D = createEmptyBandMatrix(dim, bandwidth);
+    _myTimer.startTimer();
+    cholesky_band_serial(A, L, D);
+    _myTimer.stopTimer();
+    std::cout << "Elapsed Time to Perform Cholesky Decomposition on Input SPDB Matrix : " << _myTimer.elapsedInSec() << " second\n";
 
-  /*
-  // check results
-  cout << "Comparing results... " << flush;
-  int differences = cmp_matrices(result, A);
-  if(differences == 0)
-  {
+    /*
+    // check results
+    cout << "Comparing results... " << flush;
+    int differences = cmp_matrices(result, A);
+    if(differences == 0)
+    {
     cout << "PASSED!" << endl;
-  }
-  else
-  {
+    }
+    else
+    {
     cout << "FAILED! - " << differences << " differences" << endl;
-  }
-  */
+    }
+    */
 }
